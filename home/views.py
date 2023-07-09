cfc83af9e5832a87fac30045147c64d7c96b12ea
Andre <sarreqiandrea@gmail.com>
2023-07-09 19:19:33 +0000

    Added navbar to the main page and created the views on the home folder

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.shortcuts import render
+
+
+def index(request):
+    """ A view to return the index page """
+
+    return render(request, 'home/index.html')
