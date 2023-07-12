d19d81723cfcfe4f404e92ebbb4adede35160a26
Andre <sarreqiandrea@gmail.com>
2023-07-12 22:36:53 +0000

    Created the laptops model

--- products/models.py
+++ products/models.py
@@ -17,3 +17,23 @@ class Computers(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Laptops(models.Model):
+    name = models.CharField(max_length=250, null=False, blank=False)
+    motherboard = models.CharField(max_length=250, null=False, blank=False)
+    proccessor = models.CharField(max_length=250, null=False, blank=False)
+    ram = models.CharField(max_length=250, null=False, blank=False)
+    memory = models.CharField(max_length=250, null=False, blank=False)
+    screen_size = models.CharField(max_length=50, null=False, blank=False)
+    battery = models.CharField(max_length=50, null=False, blank=False)
+    in_stock = models.BooleanField(default=False, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    description = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
