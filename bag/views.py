0b95b0818d5da837c09291d91bc72e4b0cbd8226
Andre <sarreqiandrea@gmail.com>
2023-07-19 16:57:49 +0000

    Added messages when an item is added to the bag

--- bag/views.py
+++ bag/views.py
@@ -16,14 +16,17 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a product to the shopping bag """
 
+    computer = get_object_or_404(Computers, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Added {computer.name} to your bag')
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {computer.name} to your bag')
 
     request.session['bag'] = bag
 
