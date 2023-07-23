2df851df7bbb8b993ff94b37377265d58b20a956
Andre <sarreqiandrea@gmail.com>
2023-07-23 16:42:16 +0000

    Created views for contact model and for pc technician model

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,72 @@
 from django.shortcuts import render
+from contacts.models import Contact, PcTechnician
 
-# Create your views here.
+
+class ContactUs(View):
+    """
+    The ContactUs view which allows the user to send a message to the admin
+    """
+
+    model = Contact
+    template_name = 'contact.html'
+    form_class = ContactForm
+    success_url = '/'
+
+    def get(self, request):
+        """
+        Gets the actual form to display on the page.
+        """
+
+        context = {'form': ContactForm()}
+        return render(request, 'contact.html', context)
+
+    def post(self, request):
+        """
+        This function actually posts the data, sends the data to the database.
+        """
+        if request.method == "POST":
+            form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                self.request,
+                'Thank you! Your message has been sent successfully.')
+            return redirect('/')
+        else:
+            form = ContactForm()
+        return render(request, 'contact.html', context)
+
+
+class ContactTechnician(View):
+    """
+    The ContactUs view which allows the user to send a message to the admin
+    """
+
+    model = PcTechnician
+    template_name = 'pc_technician.html'
+    form_class = ContactForm
+    success_url = '/'
+
+    def get(self, request):
+        """
+        Gets the actual form to display on the page.
+        """
+
+        context = {'form': PcTechnicianForm()}
+        return render(request, 'pc_technician.html', context)
+
+    def post(self, request):
+        """
+        This function actually posts the data, sends the data to the database.
+        """
+        if request.method == "POST":
+            form = PcTechnicianForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                self.request,
+                'Thank you! Your message has been sent successfully.')
+            return redirect('/')
+        else:
+            form = PcTechnicianForm()
+        return render(request, 'pc_technician.html', context)
