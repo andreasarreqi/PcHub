1c1d4f9bdd2694df13781bb8aa8dbd95215562c5
Andre <sarreqiandrea@gmail.com>
2023-07-24 18:43:22 +0000

    Created  contact and pc technician admin panels

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Contact, PcTechnician
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactPage(admin.ModelAdmin):
+    list_display = ('name', 'email', 'message', 'sent')
+    search_fields = ['name', 'message']
+
+
+@admin.register(PcTechnician)
+class Technician(admin.ModelAdmin):
+    list_display = ('name', 'email', 'message', 'sent')
+    search_fields = ['name', 'message']
